772999a7e96ffe08a88e12acee85d1eee2e14b1a
sorezvani <s04rezvani@gmail.com>
2025-07-03 23:29:54 +0330

    make the database prestaince

--- timesheet/settings.py
+++ timesheet/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jalali',
     'timer',
 ]
 
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'timesheet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 
